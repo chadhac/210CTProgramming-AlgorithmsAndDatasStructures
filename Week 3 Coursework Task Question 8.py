4e34f1f514670346c4358d7a50ba2d40adbfaef6
Charanjit Chadha <chadhac@uni.coventry.ac.uk>
2016-12-01 23:19:30 +0000

    Add files via upload

---
+++ Week 3 Coursework Task Question 8.py
@@ -0,0 +1,14 @@
+#Week 3
+#Question 8
+
+class removeVocals():
+    """Function to remove vocals in a word"""
+    removeVocals = ["b", "e", "a", "u", "t", "i", "f", "u", "l"] #list
+    removeVocals.remove("e") #Removes letter
+    removeVocals.remove("a") #Same as above
+    removeVocals.remove("u") #Same as above
+    removeVocals.remove("i") #Same as above
+    removeVocals.remove("u") #Same as above
+    print("Updated:",removeVocals)
+
+removeVocals()
