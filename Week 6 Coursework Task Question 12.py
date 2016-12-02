92999925a94fa53fcd9acde3bc8ab26acfad6488
Charanjit Chadha <chadhac@uni.coventry.ac.uk>
2016-12-02 21:30:38 +0000

    Add files via upload

---
+++ Week 6 Coursework Task Question 12.py
@@ -0,0 +1,31 @@
+#Week 6
+#Question 12
+
+#Tree sort algorithm
+#Pre order
+#In order
+#Post order
+
+class treeSort():
+    """Function to implement tree sort algorithm"""
+
+def preOrder():
+    if self.left_child:
+        self.left_child.preorder()
+    if self.right_child:
+        self.right_child.preorder()
+    
+def inOrder():
+    if treeSort != None:
+        order(treeSort.left_child())
+        order(tree.right_child())
+        print(tree.root_val())
+    
+def postOrder():
+    if treeSort != None:
+        order(treeSort.left_child())
+        order(tree.right_child())
+        print(tree.root_val())
+
+
+treeSort()
