e01225425da2d4c35809c6185c3c885afcc3c3c2
Charanjit Chadha <chadhac@uni.coventry.ac.uk>
2016-12-02 19:40:10 +0000

    Add files via upload

---
+++ Week 5 Coursework Task Question 11.py
@@ -0,0 +1,21 @@
+#Week 5
+#Question 11
+
+class binarySearchtree(self):
+    """Function for the binary search tree"""
+    self.left_child = None
+    self.right_child = None
+
+binarySearchtree()
+
+def binaryRoot(binary, node):
+    if binary is 0:
+        else:
+            if binarySearchTree > binaryRoot:
+                binary.right_child is None
+                return None
+            else:
+                return right_child
+                
+    
+    
